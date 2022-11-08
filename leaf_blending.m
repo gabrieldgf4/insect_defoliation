f95a8c2c5a3321b59e9100676d4f1bf4f41b214c
Gabriel Vieira <63321757+gabrieldgf4@users.noreply.github.com>
2022-11-08 19:39:45 -0300

    Update leaf_blending.m

--- leaf_blending.m
+++ leaf_blending.m
@@ -11,8 +11,11 @@ function reconstructed_leaf  = leaf_blending(leaf_model, damaged_leaf_out)
     
     damaged_leaf_out_mask = imerode(damaged_leaf_out_mask, strel('disk', 2));
     
-    source_mask = damaged_leaf_out_mask;
-    source = damaged_leaf_out;
+%     source_mask = damaged_leaf_out_mask;
+%     source = damaged_leaf_out;
+  
+    source_mask = damaged_leaf_out_mask & leaf_model_mask; % only leaf regions that are in the leaf model
+    source = damaged_leaf_out.*leaf_model_mask; % only leaf regions that are in the leaf model
     target_mask = leaf_model_mask;
     target = leaf_model;
     reconstructed_leaf = ConvPyrBlending(target,source,source_mask, target_mask);
