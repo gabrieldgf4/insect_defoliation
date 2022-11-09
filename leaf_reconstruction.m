090d12abaeed49b852a2803fac8b04728b55eb87
Gabriel Vieira <63321757+gabrieldgf4@users.noreply.github.com>
2022-11-08 21:02:48 -0300

    Update leaf_reconstruction.m

--- leaf_reconstruction.m
+++ leaf_reconstruction.m
@@ -5,10 +5,12 @@
 
 function reconstructed_leaf  = leaf_reconstruction(leaf_model, damaged_leaf_out)
 
-    leaf_damaged_to_model_mask = logical(damaged_leaf_out(:,:,2));
-    
-    cHull = bwconvhull(leaf_damaged_to_model_mask);
+%     leaf_damaged_to_model_mask = logical(damaged_leaf_out(:,:,2));
+%     
+%     cHull = bwconvhull(leaf_damaged_to_model_mask);
+% 
+%     reconstructed_leaf = leaf_model.*cHull;
 
-    reconstructed_leaf = leaf_model.*cHull;
+reconstructed_leaf = leaf_model;
 
 end
